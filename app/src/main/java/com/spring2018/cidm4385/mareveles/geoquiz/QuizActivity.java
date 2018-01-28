94e35b10656bd7e6cd196e2fa5b9af3d4e77f8ba
mareveles <mareveles1@buffs.wtamu.edu>
2018-01-28 15:23:07 -0600

    Changed Code on QuizActivity.java

--- app/src/main/java/com/spring2018/cidm4385/mareveles/geoquiz/QuizActivity.java
+++ app/src/main/java/com/spring2018/cidm4385/mareveles/geoquiz/QuizActivity.java
@@ -6,36 +6,37 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class QuizActivity extends AppCompatActivity {
+    public class QuizActivity extends AppCompatActivity {
 
-    private Button mTrueButton;
-    private Button mFalseButton;
+        private Button mTrueButton;
+        private Button mFalseButton;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quiz);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_quiz);
 
-        mTrueButton = (Button) findViewById(R.id.true_button);
-        mTrueButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
-                        R.string.correct_toast,
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
+            mTrueButton = (Button) findViewById(R.id.true_button);
+            mTrueButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(QuizActivity.this,
+                            R.string.correct_toast,
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
 
-        mFalseButton = (Button) findViewById(R.id.false_button);
-        mFalseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
-                        R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
+            mFalseButton = (Button) findViewById(R.id.false_button);
+            mFalseButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(QuizActivity.this,
+                            R.string.incorrect_toast,
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
-}
