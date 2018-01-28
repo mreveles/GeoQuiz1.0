7baee787f49627efae335c3e4a436f2f83c457d4
mareveles <mareveles1@buffs.wtamu.edu>
2018-01-28 17:31:38 -0600

    -Added next button -Added more question -Conncected so they function in app

--- app/src/main/java/com/spring2018/cidm4385/mareveles/geoquiz/QuizActivity.java
+++ app/src/main/java/com/spring2018/cidm4385/mareveles/geoquiz/QuizActivity.java
@@ -13,19 +13,31 @@ import android.widget.Toast;
 
         private Button mTrueButton;
         private Button mFalseButton;
+        private Button mNextButton;
+        private TextView mQuestionTextView;
+        private Question[] mQuestionBank = new Question[]{
+                new Question(R.string.question_australia, true),
+                new Question(R.string.question_oceans, true),
+                new Question(R.string.question_mideast, false),
+                new Question(R.string.question_africa, false),
+                new Question(R.string.question_americas, true),
+                new Question(R.string.question_asia, true),
+        };
+
+        private int mCurrentIndex=0;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_quiz);
 
+            mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+
             mTrueButton = (Button) findViewById(R.id.true_button);
             mTrueButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Toast.makeText(QuizActivity.this,
-                            R.string.correct_toast,
-                            Toast.LENGTH_SHORT).show();
+                    checkAnswer(true);
                 }
             });
 
@@ -33,10 +45,40 @@ import android.widget.Toast;
             mFalseButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Toast.makeText(QuizActivity.this,
-                            R.string.incorrect_toast,
-                            Toast.LENGTH_SHORT).show();
+                    checkAnswer(false);
                 }
             });
+
+            mNextButton = (Button) findViewById(R.id.next_button);
+            mNextButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mCurrentIndex = (mCurrentIndex+1) % mQuestionBank.length;
+                    updateQuestion();
+                }
+            });
+
+            updateQuestion();
+        }
+
+        private void updateQuestion(){
+            int question = mQuestionBank[mCurrentIndex].getTextResId();
+            mQuestionTextView.setText(question);
+        }
+
+        private void checkAnswer(boolean userPressedTrue){
+            boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+
+            int messageResId = 0;
+
+            if (userPressedTrue == answerIsTrue){
+                messageResId = R.string.correct_toast;
+            }else{
+                messageResId = R.string.incorrect_toast;
+            }
+
+            Toast customToast = Toast.makeText(this,messageResId,Toast.LENGTH_SHORT);
+            customToast.setGravity(Gravity.TOP,0,650);
+            customToast.show();
         }
     }
